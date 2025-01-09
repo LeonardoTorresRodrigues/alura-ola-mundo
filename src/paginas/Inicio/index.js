c612b7c85c4c247d89ddc676a59468edd602f91d
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-01-09 17:20:33 -0300

    style: formatting with prettier and eslint

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,5 +1,3 @@
 export default function Inicio() {
-    return (
-        <h1>Olá mundo!</h1>
-    )
+  return <h1>Olá mundo!</h1>;
 }
--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,5 +1,3 @@
-export default function SobreMim(){
-    return(
-        <h1>Sobre Mim</h1>
-    )
+export default function SobreMim() {
+  return <h1>Sobre Mim</h1>;
 }
